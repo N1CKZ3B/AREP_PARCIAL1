9e77da758f9ec7d4ecbf65c942ae2cea491ad80c
Nicolas Achuri <143991834+N1CKZ3B@users.noreply.github.com>
2024-09-12 13:38:37 -0500

    Add files via upload

---
+++ src/main/java/edu/eci/arep/app/parcial1/Parcial1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.eci.arep.app.parcial1;
+
+/**
+ *
+ * @author NICOLAS.ACHURI-M
+ */
+public class Parcial1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
